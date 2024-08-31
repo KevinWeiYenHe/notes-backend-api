c7c70419fcb66eaabc30c66cc11ada6686b03784
Kevin He <kevin66he@hotmail.com>
2024-08-30 23:48:46 -0400

    Added User Activation Tokens where they are sent to email which is later required for submission to verify an account

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -23,6 +23,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/notes", app.listNotesHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
 	return app.recoverPanic(router)
 }
